0ff4eaf0da2731cc00e53474d06d44f13ba8ea1d
Nasser Mokadem <nmokadem@hotmail.com>
2022-01-06 20:04:49 +0000

    Add simple http authentication for dashboard and admin products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['auth_username'], password: ENV['auth_password']
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with  name: ENV['auth_username'], password: ENV['auth_password']
+
   def index
     @products = Product.order(id: :desc).all
   end
