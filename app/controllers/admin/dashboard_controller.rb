7326361ce69c665159343f23f965bed1ac48e3fa
Nasser Mokadem <nmokadem@hotmail.com>
2022-01-06 23:03:16 +0000

    Display Total # of products available and by categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,10 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['auth_username'], password: ENV['auth_password']
 
   def show
+    @categories = Category.select("categories.name as category_name, sum(quantity) as total_quantity")
+    .joins(:products).group("category_name")
+
+    @products = Product.sum("quantity")
   end
 end
+
